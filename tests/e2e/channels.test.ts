e951856df069680d64cac6135d808ccf6f203958
Ibra Ben <bra-95@live.fr>
2024-03-21 00:12:16 +0100

    test: add create, edit channel and send message tests

--- tests/e2e/channels.test.ts
+++ tests/e2e/channels.test.ts
@@ -1,16 +1,39 @@
 import { test, expect } from '#tests/playwright-utils';
 
-test('Users can create channels', async ({ page, login }) => {
-  await login()
+test('Users can create and edit channels', async ({ page, login }) => {
+    await login()
 
-  await page.goto('/channels');
+    await page.goto('/channels');
 
-  const newChannelName= 'new channel'
-  await page.locator('div').filter({ hasText: /^Channels$/ }).getByRole('button').click();
-  await page.getByLabel('Name').fill(newChannelName);
-  await page.getByLabel('Description').click();
-  await page.getByLabel('Description').fill('description');
-  await page.getByRole('button', { name: 'Save' }).click();
+    const newChannelName = 'new channel'
+    await page.locator('div').filter({ hasText: /^Channels$/ }).getByRole('button').click();
+    await page.getByLabel('Name').fill(newChannelName);
+    await page.getByLabel('Description').click();
+    await page.getByLabel('Description').fill('description');
+    await page.getByRole('button', { name: 'Save' }).click();
 
-  await expect(page.locator('a').getByText(newChannelName)).toBeVisible()
+    await expect(page.locator('a').getByText(newChannelName)).toBeVisible()
+
+    await page.getByRole('link', { name: newChannelName }).click();
+    const editedChannelName = `edited channel`
+    await page.getByRole('button').first().click();
+    await page.getByLabel('Name').fill(editedChannelName);
+    await page.getByLabel('Description').click();
+    await page.getByLabel('Description').fill('edited description');
+    await page.getByRole('button', { name: 'Save' }).click();
+
+    await expect(page.getByRole('complementary').getByRole('heading', { name: editedChannelName })).toBeVisible();
+});
+
+test('Users can send messages in channels', async ({ page, login, insertNewChannel }) => {
+    const user = await login()
+    const channel = await insertNewChannel({ ownerId: user.id })
+
+    await page.goto('/channels');
+    await page.getByRole('link', { name: channel.name }).click();
+    await page.getByRole('textbox').click();
+    await page.getByRole('textbox').fill('hey there');
+    await page.locator('button[name="intent"]').click();
+
+    await expect(page.locator('div').getByText('hey there')).toBeVisible();
 });
--- tests/playwright-utils.ts
+++ tests/playwright-utils.ts
@@ -1,99 +1,149 @@
 import { test as base } from '@playwright/test'
-import { type User as UserModel } from '@prisma/client'
+import { type User as UserModel, type Channel as ChannelModel } from '@prisma/client'
 import * as setCookieParser from 'set-cookie-parser'
 import {
-	getPasswordHash,
-	getSessionExpirationDate,
-	sessionKey,
+    getPasswordHash,
+    getSessionExpirationDate,
+    sessionKey,
 } from '#app/utils/auth.server.ts'
 import { prisma } from '#app/utils/db.server.ts'
 import { authSessionStorage } from '#app/utils/session.server.ts'
 import { createUser } from './db-utils.ts'
+import { faker } from '@faker-js/faker'
 
 export * from './db-utils.ts'
 
 type GetOrInsertUserOptions = {
-	id?: string
-	username?: UserModel['username']
-	password?: string
-	email?: UserModel['email']
+    id?: string
+    username?: UserModel['username']
+    password?: string
+    email?: UserModel['email']
 }
 
 type User = {
-	id: string
-	email: string
-	username: string
-	name: string | null
+    id: string
+    email: string
+    username: string
+    name: string | null
+}
+
+type Channel = Pick<ChannelModel, 'id' | 'name' | 'description' | 'ownerId'> & { isPrivate?: boolean }
+type GetOrInsertChannelOptions = Partial<Channel>
+
+async function getOrInsertChannel({
+    id,
+    name,
+    description,
+    ownerId,
+    isPrivate,
+}: GetOrInsertChannelOptions = {}): Promise<Channel> {
+    const select = { id: true, name: true, description: true, ownerId: true }
+    if (id) {
+        return await prisma.channel.findUniqueOrThrow({
+            select,
+            where: { id: id },
+        })
+    } else {
+        const owner = await getOrInsertUser({ ...(ownerId ? { id: ownerId } : {}) })
+        name ??= faker.word.noun()
+        description ??= ""
+        isPrivate ??= false
+        return await prisma.channel.create({
+            select,
+            data: {
+                name,
+                description,
+                ownerId: owner.id,
+                ...(!isPrivate
+                    ? {}
+                    : {
+                        private: {
+                            create: {},
+                        },
+                    }),
+            },
+        })
+    }
 }
 
 async function getOrInsertUser({
-	id,
-	username,
-	password,
-	email,
+    id,
+    username,
+    password,
+    email,
 }: GetOrInsertUserOptions = {}): Promise<User> {
-	const select = { id: true, email: true, username: true, name: true }
-	if (id) {
-		return await prisma.user.findUniqueOrThrow({
-			select,
-			where: { id: id },
-		})
-	} else {
-		const userData = createUser()
-		username ??= userData.username
-		password ??= userData.username
-		email ??= userData.email
-		return await prisma.user.create({
-			select,
-			data: {
-				...userData,
-				email,
-				username,
-				roles: { connect: { name: 'user' } },
-				password: { create: { hash: await getPasswordHash(password) } },
-			},
-		})
-	}
+    const select = { id: true, email: true, username: true, name: true }
+    if (id) {
+        return await prisma.user.findUniqueOrThrow({
+            select,
+            where: { id: id },
+        })
+    } else {
+        const userData = createUser()
+        username ??= userData.username
+        password ??= userData.username
+        email ??= userData.email
+        return await prisma.user.create({
+            select,
+            data: {
+                ...userData,
+                email,
+                username,
+                roles: { connect: { name: 'user' } },
+                password: { create: { hash: await getPasswordHash(password) } },
+            },
+        })
+    }
 }
 
 export const test = base.extend<{
-	insertNewUser(options?: GetOrInsertUserOptions): Promise<User>
-	login(options?: GetOrInsertUserOptions): Promise<User>
+    insertNewUser(options?: GetOrInsertUserOptions): Promise<User>
+    insertNewChannel(options?: GetOrInsertChannelOptions): Promise<Channel>
+    login(options?: GetOrInsertUserOptions): Promise<User>
 }>({
-	insertNewUser: async ({}, use) => {
-		let userId: string | undefined = undefined
-		await use(async options => {
-			const user = await getOrInsertUser(options)
-			userId = user.id
-			return user
-		})
-		await prisma.user.delete({ where: { id: userId } }).catch(() => {})
-	},
-	login: async ({ page }, use) => {
-		let userId: string | undefined = undefined
-		await use(async options => {
-			const user = await getOrInsertUser(options)
-			userId = user.id
-			const session = await prisma.session.create({
-				data: {
-					expirationDate: getSessionExpirationDate(),
-					userId: user.id,
-				},
-				select: { id: true },
-			})
+    insertNewUser: async ({ }, use) => {
+        let userId: string | undefined = undefined
+        await use(async options => {
+            const user = await getOrInsertUser(options)
+            userId = user.id
+            return user
+        })
+        await prisma.user.delete({ where: { id: userId } }).catch(() => { })
+    },
+    insertNewChannel: async ({ }, use) => {
+        let channelId: string | undefined = undefined
+        await use(async options => {
+            const channel = await getOrInsertChannel(options)
+            channelId = channel.id
+            return channel
+        })
+        await prisma.channel.delete({ where: { id: channelId } }).catch(() => { })
+    },
+    login: async ({ page }, use) => {
+        let userId: string | undefined = undefined
+        await use(async options => {
+            const user = await getOrInsertUser(options)
+            userId = user.id
+            const session = await prisma.session.create({
+                data: {
+                    expirationDate: getSessionExpirationDate(),
+                    userId: user.id,
+                },
+                select: { id: true },
+            })
 
-			const authSession = await authSessionStorage.getSession()
-			authSession.set(sessionKey, session.id)
-			const cookieConfig = setCookieParser.parseString(
-				await authSessionStorage.commitSession(authSession),
-			) as any
-			await page
-				.context()
-				.addCookies([{ ...cookieConfig, domain: 'localhost' }])
-			return user
-		})
-		await prisma.user.deleteMany({ where: { id: userId } })
-	},
+            const authSession = await authSessionStorage.getSession()
+            authSession.set(sessionKey, session.id)
+            const cookieConfig = setCookieParser.parseString(
+                await authSessionStorage.commitSession(authSession),
+            ) as any
+            await page
+                .context()
+                .addCookies([{ ...cookieConfig, domain: 'localhost' }])
+            return user
+        })
+        await prisma.user.deleteMany({ where: { id: userId } })
+    },
 })
 export const { expect } = test
 
@@ -105,22 +155,22 @@ export const { expect } = test
  * throw the error message provided as a fallback
  */
 export async function waitFor<ReturnValue>(
-	cb: () => ReturnValue | Promise<ReturnValue>,
-	{
-		errorMessage,
-		timeout = 5000,
-	}: { errorMessage?: string; timeout?: number } = {},
+    cb: () => ReturnValue | Promise<ReturnValue>,
+    {
+        errorMessage,
+        timeout = 5000,
+    }: { errorMessage?: string; timeout?: number } = {},
 ) {
-	const endTime = Date.now() + timeout
-	let lastError: unknown = new Error(errorMessage)
-	while (Date.now() < endTime) {
-		try {
-			const response = await cb()
-			if (response) return response
-		} catch (e: unknown) {
-			lastError = e
-		}
-		await new Promise(r => setTimeout(r, 100))
-	}
-	throw lastError
+    const endTime = Date.now() + timeout
+    let lastError: unknown = new Error(errorMessage)
+    while (Date.now() < endTime) {
+        try {
+            const response = await cb()
+            if (response) return response
+        } catch (e: unknown) {
+            lastError = e
+        }
+        await new Promise(r => setTimeout(r, 100))
+    }
+    throw lastError
 }
